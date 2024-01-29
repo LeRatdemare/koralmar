2f162b983ae9dc8405557ce21393b94d9bd7fe97
Nathan Lufuluabo <lufuluabon@outlook.fr>
2024-01-29 11:27:26 +0100

    Déplacement du dossier media/

--- koralmar/koralmar/settings.py
+++ koralmar/koralmar/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'calapp/static'
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'calapp/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
